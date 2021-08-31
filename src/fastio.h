f9f020dc07e7d3654b6d620cfa17a08faf9616a1
Luthaf <luthaf@luthaf.fr>
2021-08-31 10:53:32 +0200

    Allow file sharing when opening files for reading on windows

--- src/fastio.h
+++ src/fastio.h
@@ -25,11 +25,11 @@
 #define FIO_READ    0x01
 #define FIO_WRITE   0x02
 #define FIO_DIRECT  0x04 /* emulate Unix O_DIRECT flag */
- 
+
 /* Compiling on windows */
 #if defined(_MSC_VER) || defined(__MINGW32__)
 
-#if 1 
+#if 1
 /* use native Windows I/O calls */
 #define FASTIO_NATIVEWIN32 1
 
@@ -54,10 +54,10 @@ typedef struct {
 static int fio_win32convertfilename(const char *filename, char *newfilename, int maxlen) {
   int i;
   int len=strlen(filename);
- 
+
   if ((len + 1) >= maxlen)
     return -1;
-   
+
   for (i=0; i<len; i++) {
     if (filename[i] == '/')
       newfilename[i] = '\\';
@@ -79,7 +79,7 @@ static int fio_open(const char *filename, int mode, fio_fd *fd) {
   DWORD flags;
 
   if (fio_win32convertfilename(filename, winfilename, sizeof(winfilename)))
-    return -1;  
+    return -1;
 
   access = 0;
   if (mode & FIO_READ)
@@ -99,16 +99,18 @@ static int fio_open(const char *filename, int mode, fio_fd *fd) {
     return -1; /* not supported yet */
 #endif
 
-  sharing = 0;       /* disallow sharing with other processes  */
   security = NULL;   /* child processes don't inherit anything */
 
   /* since we never append, blow away anything that's already there */
-  if (mode & FIO_WRITE)
+  if (mode & FIO_WRITE) {
     createmode = CREATE_ALWAYS;
-  else 
+    sharing = 0; /* when writing, disallow sharing with other processes  */
+  } else {
     createmode = OPEN_EXISTING;
+    sharing = FILE_SHARE_READ;
+  }
 
-  fp = CreateFile(winfilename, access, sharing, security, 
+  fp = CreateFile(winfilename, access, sharing, security,
                   createmode, flags, NULL);
 
   if (fp == NULL) {
@@ -123,13 +125,13 @@ static int fio_open(const char *filename, int mode, fio_fd *fd) {
 static int fio_fclose(fio_fd fd) {
   BOOL rc;
   rc = CloseHandle(fd);
-  if (rc) 
+  if (rc)
     return 0;
-  else 
+  else
     return -1;
 }
 
-static fio_size_t fio_fread(void *ptr, fio_size_t size, 
+static fio_size_t fio_fread(void *ptr, fio_size_t size,
                             fio_size_t nitems, fio_fd fd) {
   BOOL rc;
   DWORD len;
@@ -141,7 +143,7 @@ static fio_size_t fio_fread(void *ptr, fio_size_t size,
   if (rc) {
     if (readlen == len)
       return nitems;
-    else 
+    else
       return 0;
   } else {
     return 0;
@@ -150,7 +152,7 @@ static fio_size_t fio_fread(void *ptr, fio_size_t size,
 
 static fio_size_t fio_readv(fio_fd fd, const fio_iovec * iov, int iovcnt) {
   int i;
-  fio_size_t len = 0; 
+  fio_size_t len = 0;
 
   for (i=0; i<iovcnt; i++) {
     fio_size_t rc = fio_fread(iov[i].iov_base, iov[i].iov_len, 1, fd);
@@ -162,14 +164,14 @@ static fio_size_t fio_readv(fio_fd fd, const fio_iovec * iov, int iovcnt) {
   return len;
 }
 
-static fio_size_t fio_fwrite(void *ptr, fio_size_t size, 
+static fio_size_t fio_fwrite(void *ptr, fio_size_t size,
                              fio_size_t nitems, fio_fd fd) {
   BOOL rc;
   DWORD len;
   DWORD writelen;
 
-  len = size * nitems; 
- 
+  len = size * nitems;
+
   rc = WriteFile(fd, ptr, len, &writelen, NULL);
   if (rc) {
     if (writelen == len)
@@ -199,7 +201,7 @@ static fio_size_t fio_fseek(fio_fd fd, fio_size_t offset, int whence) {
     if (GetLastError() != ERROR_SUCCESS) {
       return -1;
     }
-  } 
+  }
 
   finaloffset = finalint.QuadPart;
   return 0;
@@ -210,7 +212,7 @@ static fio_size_t fio_fseek(fio_fd fd, fio_size_t offset, int whence) {
   /* SetFilePointerEx() only exists with new .NET compilers */
   rc = SetFilePointerEx(fd, offset, &finaloffset, whence);
 
-  if (rc) 
+  if (rc)
     return 0;
   else
     return -1;
@@ -267,15 +269,15 @@ static int fio_open(const char *filename, int mode, fio_fd *fd) {
   char * modestr;
   FILE *fp;
 
-  if (mode & FIO_READ) 
+  if (mode & FIO_READ)
     modestr = "rb";
 
-  if (mode & FIO_WRITE) 
+  if (mode & FIO_WRITE)
     modestr = "wb";
 
   if (mode & FIO_DIRECT)
     return -1; /* not supported yet */
- 
+
   fp = fopen(filename, modestr);
   if (fp == NULL) {
     return -1;
@@ -289,14 +291,14 @@ static int fio_fclose(fio_fd fd) {
   return fclose(fd);
 }
 
-static fio_size_t fio_fread(void *ptr, fio_size_t size, 
+static fio_size_t fio_fread(void *ptr, fio_size_t size,
                             fio_size_t nitems, fio_fd fd) {
   return fread(ptr, size, nitems, fd);
 }
 
 static fio_size_t fio_readv(fio_fd fd, const fio_iovec * iov, int iovcnt) {
   int i;
-  fio_size_t len = 0; 
+  fio_size_t len = 0;
 
   for (i=0; i<iovcnt; i++) {
     fio_size_t rc = fread(iov[i].iov_base, iov[i].iov_len, 1, fd);
@@ -308,7 +310,7 @@ static fio_size_t fio_readv(fio_fd fd, const fio_iovec * iov, int iovcnt) {
   return len;
 }
 
-static fio_size_t fio_fwrite(void *ptr, fio_size_t size, 
+static fio_size_t fio_fwrite(void *ptr, fio_size_t size,
                              fio_size_t nitems, fio_fd fd) {
   return fwrite(ptr, size, nitems, fd);
 }
@@ -322,7 +324,7 @@ static fio_size_t fio_ftell(fio_fd fd) {
 }
 #endif /* plain ANSI C */
 
-#else 
+#else
 
 /* Version for UNIX machines */
 #if defined(__linux)
@@ -343,14 +345,14 @@ typedef off_t fio_size_t;      /* off_t is 64-bits with LFS builds */
 /*
  * Enable use of kernel readv() if available and reliable
  *
- * Note: Some Linux implementations incorporate readv() code in libc 
+ * Note: Some Linux implementations incorporate readv() code in libc
  * that does userspace copying of I/O vectors to internal temporary
  * buffers in order to meet the atomicity requirements of the POSIX standard.
  * Such copies make the use of vectorized I/O APIs much less useful for
  * large trajectory files because the internal buffer allocations can fail
  * badly when performing large aggregate I/O operations.  It may be that
  * other implementations of vector I/O have similar problems, and in these
- * cases it is probably best not to use it at all, and to fall back to 
+ * cases it is probably best not to use it at all, and to fall back to
  * non-vectorized I/O APIs to avoid such extra copies.
  */
 #if defined(__sun) || defined(__APPLE_CC__) || defined(__linux)
@@ -378,11 +380,11 @@ typedef struct {
 static int fio_open(const char *filename, int mode, fio_fd *fd) {
   int nfd;
   int oflag = 0;
-  
-  if (mode & FIO_READ) 
+
+  if (mode & FIO_READ)
     oflag = O_RDONLY;
 
-  if (mode & FIO_WRITE) 
+  if (mode & FIO_WRITE)
     oflag = O_WRONLY | O_CREAT | O_TRUNC;
 
 #if defined(__linux)
@@ -407,19 +409,19 @@ static int fio_fclose(fio_fd fd) {
   return close(fd);
 }
 
-static fio_size_t fio_fread(void *ptr, fio_size_t size, 
+static fio_size_t fio_fread(void *ptr, fio_size_t size,
                             fio_size_t nitems, fio_fd fd) {
   fio_size_t i;
-  fio_size_t len = 0; 
+  fio_size_t len = 0;
   fio_size_t cnt = 0;
 
 #if 1
   /*
    * On Linux individual calls to read() can end up doing short reads when
-   * reading more than 2GB in a single read call, even on 64-bit machines.  
+   * reading more than 2GB in a single read call, even on 64-bit machines.
    * For large structures, e.g. 240M-atoms or larger, we have to use a loop
    * to continue reading into the memory buffer until completion.
-   */ 
+   */
   for (i=0; i<nitems; i++) {
     fio_size_t szleft = size;
     fio_size_t rc = 0;
@@ -474,13 +476,13 @@ static fio_size_t fio_readv(fio_fd fd, const fio_iovec * iov, int iovcnt) {
   }
 
   if ((len < 0 && errno == ENOSYS) ||
-      (len != tlen && errno == EINVAL)) 
+      (len != tlen && errno == EINVAL))
 #endif
   {
     /* XXX this loop doesn't meet the atomicity requirements of
-     *     real POSIX readv(), since we don't need that feature 
+     *     real POSIX readv(), since we don't need that feature
      */
-    len = 0; 
+    len = 0;
     for (i=0; i<iovcnt; i++) {
       void *ptr = iov[i].iov_base;
       fio_size_t sz = iov[i].iov_len;
@@ -511,9 +513,9 @@ static fio_size_t fio_readv(fio_fd fd, const fio_iovec * iov, int iovcnt) {
 #endif
   {
     /* XXX this loop doesn't meet the atomicity requirements of
-     *     real POSIX readv(), since we don't need that feature 
+     *     real POSIX readv(), since we don't need that feature
      */
-    len = 0; 
+    len = 0;
     for (i=0; i<iovcnt; i++) {
       fio_size_t rc = read(fd, iov[i].iov_base, iov[i].iov_len);
       if (rc != iov[i].iov_len)
@@ -526,19 +528,19 @@ static fio_size_t fio_readv(fio_fd fd, const fio_iovec * iov, int iovcnt) {
   return len;
 }
 
-static fio_size_t fio_fwrite(void *ptr, fio_size_t size, 
+static fio_size_t fio_fwrite(void *ptr, fio_size_t size,
                              fio_size_t nitems, fio_fd fd) {
   fio_size_t i;
-  fio_size_t len = 0; 
+  fio_size_t len = 0;
   fio_size_t cnt = 0;
 
 #if 1
   /*
    * On Linux individual calls to write() can end up doing short writes when
-   * writing more than 2GB in a single write call, even on 64-bit machines.  
+   * writing more than 2GB in a single write call, even on 64-bit machines.
    * For large structures, e.g. 240M-atoms or larger, we have to use a loop
    * to continue writing the memory buffer until completion.
-   */ 
+   */
   int writecalls=0;
   for (i=0; i<nitems; i++) {
     fio_size_t szleft = size;
@@ -557,7 +559,7 @@ static fio_size_t fio_fwrite(void *ptr, fio_size_t size,
       writecalls++;
       rc = write(fd, ((char*) ptr)+(size-szleft), writesz);
       if (rc < 0) {
-        printf("fio_fwrite(): rc %ld  sz: %ld  szleft: %ld  calls: %d\n", 
+        printf("fio_fwrite(): rc %ld  sz: %ld  szleft: %ld  calls: %d\n",
                rc, size, szleft, writecalls);
         perror("  perror fio_fwrite(): ");
         return cnt;
@@ -585,7 +587,7 @@ static fio_size_t fio_fwrite(void *ptr, fio_size_t size,
 static fio_size_t fio_fseek(fio_fd fd, fio_size_t offset, int whence) {
  if (lseek(fd, offset, whence) >= 0)
    return 0;  /* success (emulate behavior of fseek) */
- else 
+ else
    return -1; /* failure (emulate behavior of fseek) */
 }
 
@@ -610,4 +612,3 @@ static int fio_write_str(fio_fd fd, const char *str) {
   int len = strlen(str);
   return (fio_fwrite((void *) str, len, 1, fd) != 1);
 }
-
